c6054f7e5bacafc04588f0592ea6a6cf4966435c
Ahmed Mohamad <rashin2604@gmail.com>
2024-08-16 13:55:26 +0200

    backend initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import './db/index.js';
+import cors from 'cors';
+import './models/index.js';
+const app = express();
+
+const PORT=process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
