a454149600a13f08fd12eb4bb8251b29d57d9974
Ahmed Mohamad <rashin2604@gmail.com>
2024-08-29 15:29:24 +0200

    added sugnedin user reviews route

--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getReviews, createReview, getReview,updateReview,deleteReview } from "../controllers/reviewsController.js";
+import { getReviews, createReview, getReview,updateReview,deleteReview,getReviewsByUser } from "../controllers/reviewsController.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import reviewsSchema from "../schemas/reviewsSchema.js";
 import verifyToken from "../middlewares/verifyToken.js";
@@ -8,6 +8,7 @@ const reviewRouter = Router();
 
 
 reviewRouter.get("/", getReviews);
+reviewRouter.get("/user",verifyToken, getReviewsByUser);
 reviewRouter.get("/:gpsCode", getReview);
 reviewRouter.post("/",validateSchema(reviewsSchema),verifyToken, createReview);
 reviewRouter.put("/:gpscode",validateSchema(reviewsSchema),verifyToken, updateReview);
