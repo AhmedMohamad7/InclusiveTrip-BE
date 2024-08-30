085782feb8d1fe1ef3d2fb9a752cbc22e62e734b
Ahmed Mohamad <rashin2604@gmail.com>
2024-08-30 12:27:38 +0200

    added place category selected route

--- controllers/placeCategoriesController.js
+++ controllers/placeCategoriesController.js
@@ -11,22 +11,28 @@ export const getPlaceCategories = async (req, res) => {
 }
 
 export const getPlaceCategory = async (req, res) => {
-    const { type } = req.params;
+    const { name } = req.params;
     try {
-        if (!type) throw new Error("please provide type in the params");
-        const placeCategoryToGet = await placeCategory.findOne({ where: { type } });
+        if (!name) throw new Error("please provide type in the params");
+        const placeCategoryToGet = await placeCategory.findOne({ where: { name } });
         if (!placeCategoryToGet) throw new Error("No place category found.");
         res.status(200).json(placeCategoryToGet);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 }
-
-export const createPlaceCategory = async (req, res) => {
-    const { type } = req.body;
+export const getSelectedPlaceCategories = async (req, res) => {
     try {
-        if (!type) throw new Error("please provide type in the body");
-        const newPlaceCategory = await placeCategory.create({ type});
+        const placeCategories = await placeCategory.findAll({ where: { selected: true } });
+        res.status(200).json(placeCategories);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+export const createPlaceCategory = async (req, res) => {
+    const {name,icon,description,selected} = req.body;
+    try {
+        const newPlaceCategory = await placeCategory.create({ name,icon,description,selected });
         res.status(201).json(newPlaceCategory);
     } catch (error) {
         res.status(409).json({ message: error.message });
@@ -35,11 +41,11 @@ export const createPlaceCategory = async (req, res) => {
 
 export const updatePlaceCategory = async (req, res) => {
     const { id } = req.params;
-    const { type } = req.body;
+    const { name,icon,description,selected } = req.body;
     try {
-        if (!type) throw new Error("please provide type in the body");
+        if (!name) throw new Error("please provide type in the body");
         if (!id) throw new Error("please provide id in the params");
-        const updatedPlaceCategory = await placeCategory.update({ type }, { where: { id } });
+        const updatedPlaceCategory = await placeCategory.update({ name,icon,description,selected }, { where: { id } });
         if (!updatedPlaceCategory) throw new Error("An error occurred while updating the place category."); 
         res.status(200).json(updatedPlaceCategory);
     } catch (error) {
