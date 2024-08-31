1bb120c583ac654f059ec60c907c75cb8e4f272e
adnanabdalrahman <adnan.24150@gmail.com>
2024-08-31 09:49:56 +0200

    Schema validatio, update table placesCategories

--- models/index.js
+++ models/index.js
@@ -8,20 +8,21 @@ import user from "./usersModel.js";
 import role from "./rolesModel.js";
 
 
-user.hasMany(review, {foreignKey: "userId"});
-review.belongsTo(user, {foreignKey: "userId"});
+user.hasMany(review, { foreignKey: "userId" });
+review.belongsTo(user, { foreignKey: "userId" });
 
-role.hasOne(user, {foreignKey: "roleId"});
-user.belongsTo(role, {foreignKey: "roleId"});
+role.hasOne(user, { foreignKey: "roleId" });
+user.belongsTo(role, { foreignKey: "roleId" });
 
-review.hasMany(fileUpload, {foreignKey: "reviewId"});
-fileUpload.belongsTo(review, {foreignKey: "reviewId"});
+review.hasMany(fileUpload, { foreignKey: "reviewId" });
+fileUpload.belongsTo(review, { foreignKey: "reviewId" });
 
-placeCategory.hasMany(review, {foreignKey: "placeCategoriesId"});
-review.belongsTo(placeCategory, {foreignKey: "placeCategoriesId"});
+placeCategory.hasMany(review, { foreignKey: "placeCategoriesId" });
+review.belongsTo(placeCategory, { foreignKey: "placeCategoriesId" });
 
-review.belongsToMany(barrier, {through: barrierReview, foreignKey: "reviewId"});
-barrier.belongsToMany(review, {through: barrierReview, foreignKey: "barrierId"});
+review.belongsToMany(barrier, { through: barrierReview, foreignKey: "reviewId" });
+barrier.belongsToMany(review, { through: barrierReview, foreignKey: "barrierId" });
 
 await sequelize.sync();
 console.log("All models were synchronized successfully.");
+
--- schemas/reviewsSchema.js
+++ schemas/reviewsSchema.js
@@ -2,10 +2,20 @@ import Joi from "joi";
 import user from "../models/usersModel.js";
 
 const reviewsSchema = Joi.object({
-    gpsCode: Joi.string().required(),
-    comment : Joi.string().required(),
-    placeCategoriesId: Joi.number().required(),
-
+    gpsCode: Joi.string().required().messages({
+        'string.base': 'GPS code must be a string',
+        'string.empty': 'GPS code is required',
+        'any.required': 'GPS code is required'
+    }),
+    comment: Joi.string().required().messages({
+        'string.base': 'Comment must be a string',
+        'string.empty': 'Comment is required',
+        'any.required': 'Comment is required'
+    }),
+    placeCategoriesId: Joi.number().required().messages({
+        'number.base': 'Place category ID must be a number',
+        'any.required': 'Place category ID is required'
+    })
 });
 
 export default reviewsSchema;
