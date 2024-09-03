1d37c0cf1bcb61e4d476cc5fc2a71795485baee3
adnanabdalrahman <adnan.24150@gmail.com>
2024-09-03 14:51:42 +0200

    BarrierReview

--- routes/barriersReviewsRoute.js
+++ routes/barriersReviewsRoute.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { getBarriersReviews , getBarrierReview, createBarrierReview, updateBarrierReview, deleteBarrierReview} from "../controllers/barriersReviewsController.js";
+import { getBarrierReviewsByReviewId, getBarriersReviews, getBarrierReview, createBarrierReview, updateBarrierReview, deleteBarrierReview } from "../controllers/barriersReviewsController.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import barriersReviewsSchema from "../schemas/barriersReviewsSchema.js";
 
@@ -9,9 +9,10 @@ const barrierReviewRouter = Router();
 
 barrierReviewRouter.get("/", getBarriersReviews);
 barrierReviewRouter.get("/:id", getBarrierReview);
-barrierReviewRouter.post("/",validateSchema(barriersReviewsSchema), createBarrierReview);
-barrierReviewRouter.put("/:id",validateSchema(barriersReviewsSchema), updateBarrierReview);
+barrierReviewRouter.post("/", validateSchema(barriersReviewsSchema), createBarrierReview);
+barrierReviewRouter.put("/:id", validateSchema(barriersReviewsSchema), updateBarrierReview);
 barrierReviewRouter.delete("/:id", deleteBarrierReview);
+barrierReviewRouter.get("/review/:reviewId", getBarrierReviewsByReviewId);
 
 
 export default barrierReviewRouter;
